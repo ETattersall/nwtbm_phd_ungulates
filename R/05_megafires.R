96fe4b99b97e052dadae9dba50d5dccef67da755
Erin Tattersall <ertattersall@gmail.com>
2026-03-20 14:00:18 -0700

    Updating megafires histogram, adding megafires plot

--- R/05_megafires.R
+++ R/05_megafires.R
@@ -35,7 +35,11 @@ sa_20km_fires <- st_read("C:/Users/tatterer.stu/Desktop/nwtbm_phd_ungulates/data
 
 ## Camera locations (not buffered)
 cam_locs_sf <- st_read("data/wt_location_data/all_projects_cam_locations_20260310.shp")
+glimpse(cam_locs_sf) ## column names have gotten all mixed up - fix 
+colnames(cam_locs_sf) <- c("location", "buffer_m", "location_visibility", "true_coordinates", "location_comments", "internal_wildtrax_id", "study_area", "geometry")
 
+## save again
+st_write(cam_locs_sf, "data/wt_location_data/all_projects_cam_locations_20260320.shp")
 
 ## Megafire years ##
 # Megafires are defined as >10 000 ha in size. Giga fires are defined as >100 000 ha in size.
@@ -80,14 +84,15 @@ hist(sa_megafires$FireAge)
 neg.fire.age <- sa_megafires[sa_megafires$FireAge < 0, ]
 neg.fire.age <- neg.fire.age %>% select(NFIREID, study_area, ADJ_HA, size_group, AG_SDATE, YEAR, Year0, FireAge, Shape_Leng, Shape_Area, geometry) 
 
-## Can remove all the -3 years, and the Sambaa K'e -2. The Norman Wells -2 might overlap with a deployment period so keep it for now
+## Can remove all these from sa_megafires
+sa_megafires <- sa_megafires[!sa_megafires$FireAge < 0 , ]
 
 ## Create a bar plot of megafires and gigafires by year
-gg_sa_megafires <- ggplot(sa_megafires, aes(x = YEAR, fill = size_group)) +
+gg_sa_megafires <- ggplot(sa_megafires, aes(x = FireAge, fill = size_group)) +
   geom_bar() +
   facet_wrap(~ study_area) +
-  labs(title = "Number of Megafires and Gigafires by Year",
-       x = "Year",
+  labs(title = "Number of Megafires and Gigafires by Time Since Fire",
+       x = "Time Since Fire",
        y = "Count of Fires",
        fill = "Fire Size") +
   theme_classic() +
@@ -99,6 +104,7 @@ gg_sa_megafires <- ggplot(sa_megafires, aes(x = YEAR, fill = size_group)) +
   theme(axis.title.y = element_text(size = 16)) +
   theme(strip.text = element_text(size = 14)) # increase facet title size
 
+win.graph()
 gg_sa_megafires
 ## Save the plot
 ggsave("figures/fire_explore/Megafires_hist_bySA.png", gg_sa_megafires, width = 12, height = 8, dpi = 300)
@@ -131,21 +137,61 @@ class(sa_gigafires)
 
 
 ### MAPPING
-# ### Map of all NWT fire history plus sensor locations (including 500m buffers for visibility)
-# gg_nwt_fires <- ggplot() +
-#   geom_sf(data = nwt_fires, aes(color = YEAR), size = 0.5) + # all NWT fire polygons
-#   geom_sf(data = cams_500m_buffer, fill = NA, color = "blue", size = 0.5) + # 500m buffers around camera locations
-#   geom_sf(data = cam_locs_sf, color = "black", size = 1) + # camera locations
-#   scale_color_gradient(low = "yellow", high = "red") + # red gradient for more recent burns
-#   labs(title = "NWT Fire History (1972 - 2024) with Camera Locations",
-#        x = "Longitude",
-#        y = "Latitude",
-#        color = "Fire Year") +
-#   theme(legend.position = "right") +
-#   coord_sf(xlim = c(-1026000, 580000), ylim = c(8100000, 9360000), expand = FALSE) + # set limits to bounding box of NWT fire layer (plus a little extra buffer)
-#   theme_classic() + 
-#   # increase size of title text, axis text, and facet titles
-#   theme(plot.title = element_text(size = 24, face = "bold", hjust = 0.5)) +
-#   theme(axis.title.x = element_text(size = 16)) +
-#   theme(axis.title.y = element_text(size = 16)) +
-#   theme(legend.title= element_text(size = 16))
+## Want to map the megafires and gigafires by study area with camera locations (faceted)
+
+## Create each map separately (in a list), then plot together using plot_grid() (cowplot package)
+
+## list of study areas
+study_areas <- unique(cam_locs_sf$study_area)
+
+## Create a function to make one plot per study area
+make_sa_plot <- function(sa) {
+  
+  # get bounding box for that study area (using sa_20km fires polygons)
+  sa_bbox <- sa_20km_fires %>%
+    dplyr::filter(study_area == sa) %>%
+    summarise() %>%
+    st_bbox()
+  
+  ggplot() +
+    geom_sf(data = sa_megafires %>% filter(study_area == sa),
+            aes(fill = FireAge), color = NA,
+            size = 0.5) +
+    
+    geom_sf(data = cam_locs_sf %>% filter(study_area == sa),
+            color = "black",
+            size = 2) +
+    
+    scale_fill_gradient(low = "red", high = "yellow", name = "Time Since Fire") +
+    
+    coord_sf(xlim = c(sa_bbox["xmin"], sa_bbox["xmax"]),
+             ylim = c(sa_bbox["ymin"], sa_bbox["ymax"]),
+             expand = FALSE) +
+    
+    labs(title = paste("Megafires in", sa),
+         x = "Longitude",
+         y = "Latitude") +
+    
+    theme_classic() +
+    theme(
+      plot.title = element_text(size = 18, face = "bold", hjust = 0.5),
+      legend.position = "right"
+    )
+}
+
+
+## Create list of ggplots
+plot_list <- lapply(study_areas, make_sa_plot)
+
+
+combined_plot <- plot_grid(plotlist = plot_list,
+                      ncol = 3)   # adjust columns as needed
+
+win.graph()
+combined_plot
+
+
+## Save the plot
+ggsave("figures/fire_explore/Megafires_map_bySA.png", combined_plot, width = 12, height = 8, dpi = 300)
+
+
